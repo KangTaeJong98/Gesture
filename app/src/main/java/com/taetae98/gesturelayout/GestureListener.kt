b99407bf29785063b2e38ef33540011e77299d7d
KangTaeJong98 <rkdxowhd98@naver.com>
2021-07-17 23:25:13 +0900

    Refactoring

--- app/src/main/java/com/taetae98/gesturelayout/GestureListener.kt
+++ app/src/main/java/com/taetae98/gesturelayout/GestureListener.kt
@@ -1,12 +1,17 @@
 package com.taetae98.gesturelayout
 
+import android.graphics.PointF
 import android.view.MotionEvent
 import android.view.View
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.sqrt
 
-class GestureListener(
-    private val dragDetector: DragDetector? = DragDetector(),
-    private val scaleDetector: ScaleDetector? = ScaleDetector()
-) : View.OnTouchListener {
+open class GestureListener : View.OnTouchListener {
+    private val point by lazy { PointF() }
+    private val vector by lazy { Vector() }
+    private var distance = 0F
+
     private var mode = Mode.NONE
 
     override fun onTouch(view: View, event: MotionEvent): Boolean {
@@ -25,10 +30,8 @@ class GestureListener(
                 onActionCancel(view, event)
             }
             MotionEvent.ACTION_MOVE -> {
-                if (mode == Mode.DRAG) {
-                    dragDetector?.onActionMove(view, event)
-                } else if (mode == Mode.SCALE) {
-                    scaleDetector?.onActionMove(view, event)
+                if (mode == Mode.DRAG || mode == Mode.SCALE) {
+                    onActionMove(view, event)
                 }
             }
         }
@@ -39,25 +42,33 @@ class GestureListener(
 
     private fun onActionDown(view: View, event: MotionEvent) {
         mode = Mode.DRAG
-        dragDetector?.onActionDown(view, event)
+        point.set(event.x, event.y)
+        onDragStart(view, event)
     }
 
     private fun onActionPointerDown(view: View, event: MotionEvent) {
         mode = Mode.SCALE
-        dragDetector?.onActionCancel(view, event)
-        scaleDetector?.onActionPointerDown(view, event)
+        vector.set(event)
+        distance = distance(event)
+
+        onDragEnd(view, event)
+        onScaleStart(view, event)
+        onRotateStart(view, event)
     }
 
     private fun onActionPointerUp(view: View, event: MotionEvent) {
         when (mode) {
             Mode.DRAG -> {
                 mode = Mode.NONE
-                dragDetector?.onActionCancel(view, event)
+                onDragEnd(view, event)
             }
             Mode.SCALE -> {
                 mode = Mode.DRAG
-                dragDetector?.onActionPointerUp(view, event)
-                scaleDetector?.onActionPointerUp(view, event)
+                val index = if (event.actionIndex == 0) 1 else 0
+                point.set(event.getX(index), event.getY(index))
+                onDragStart(view, event)
+                onScaleEnd(view, event)
+                onRotateEnd(view, event)
             }
             else -> {
                 mode = Mode.NONE
@@ -66,24 +77,114 @@ class GestureListener(
     }
 
     private fun onActionCancel(view: View, event: MotionEvent) {
-        when (mode) {
-            Mode.DRAG -> {
-                mode = Mode.NONE
-                dragDetector?.onActionCancel(view, event)
-            }
-            Mode.SCALE -> {
-                mode = Mode.DRAG
-                scaleDetector?.onActionCancel(view, event)
-            }
-            else -> {
-                mode = Mode.NONE
-            }
+        if (mode == Mode.DRAG) {
+            onDragEnd(view, event)
+        } else if (mode == Mode.SCALE) {
+            onScaleEnd(view, event)
+            onRotateEnd(view, event)
         }
 
         mode = Mode.NONE
     }
 
+    private fun onActionMove(view: View, event: MotionEvent) {
+        if (mode == Mode.DRAG) {
+            onActionDrag(view, event)
+        } else if (mode == Mode.SCALE) {
+            onActionScale(view, event)
+            onActionRotate(view, event)
+        }
+    }
+
+    private fun onActionDrag(view: View, event: MotionEvent) {
+        val distanceX = event.x - point.x
+        val distanceY = event.y - point.y
+
+        onDrag(view, event, distanceX, distanceY)
+    }
+
+    private fun onActionScale(view: View, event: MotionEvent) {
+        onScale(view, event, distance(event) / distance)
+    }
+
+    private fun onActionRotate(view: View, event: MotionEvent) {
+        onRotate(view, event, view.rotation + Vector.getDegree(vector, Vector(event)))
+    }
+
+    private fun distance(event: MotionEvent): Float {
+        val x = event.getX(0) - event.getX(1)
+        val y = event.getY(0) - event.getY(1)
+
+        return sqrt(x*x + y*y)
+    }
+
+    open fun onDrag(view: View, event: MotionEvent, distanceX: Float, distanceY: Float) {
+        val array = arrayOf(distanceX, distanceY).toFloatArray()
+
+        view.matrix.mapVectors(array)
+        view.translationX += array.first()
+        view.translationY += array.last()
+    }
+
+    open fun onDragStart(view: View, event: MotionEvent) {
+
+    }
+
+    open fun onDragEnd(view: View, event: MotionEvent) {
+
+    }
+
+    open fun onScaleStart(view: View, event: MotionEvent) {
+
+    }
+
+    open fun onScale(view: View, event: MotionEvent, scale: Float) {
+        view.scaleX *= scale
+        view.scaleY *= scale
+    }
+
+    open fun onScaleEnd(view: View, event: MotionEvent) {
+
+    }
+
+    open fun onRotateStart(view: View, event: MotionEvent) {
+
+    }
+
+    open fun onRotate(view: View, event: MotionEvent, rotation: Float) {
+        view.rotation = rotation
+    }
+
+    open fun onRotateEnd(view: View, event: MotionEvent) {
+
+    }
+
     enum class Mode {
         NONE, DRAG, SCALE
+    }
+
+    class Vector(x: Float = 0F, y: Float = 0F) : PointF(x, y) {
+        companion object {
+            fun getDegree(v1: Vector, v2: Vector): Float {
+                return (180.0 / PI * (atan2(v2.y, v2.x) - atan2(v1.y, v1.x))).toFloat()
+            }
+        }
+
+        constructor(event: MotionEvent) : this() {
+            set(event)
+        }
+
+        fun set(event: MotionEvent) {
+            x = event.getX(1) - event.getX(0)
+            y = event.getY(1) - event.getY(0)
+            reduction()
+        }
+
+        private fun reduction() {
+            sqrt(x*x + y*y).also {
+                x /= it
+                y /= it
+            }
+        }
     }
 }
--- app/src/main/java/com/taetae98/gesturelayout/activity/MainActivity.kt
+++ app/src/main/java/com/taetae98/gesturelayout/activity/MainActivity.kt
@@ -5,10 +5,8 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
-import com.taetae98.gesturelayout.DragDetector
 import com.taetae98.gesturelayout.GestureListener
 import com.taetae98.gesturelayout.R
-import com.taetae98.gesturelayout.ScaleDetector
 import com.taetae98.gesturelayout.base.BindingActivity
 import com.taetae98.gesturelayout.databinding.ActivityMainBinding
 import com.taetae98.gesturelayout.viewmodel.MainActivityViewModel
@@ -28,65 +26,57 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     }
 
     private fun onCreateOnGestureListener() {
-        binding.parent.listener = GestureListener(
-            DragDetector(
-                object : DragDetector.GestureDragListener() {
-                    override fun onDrag(view: View, event: MotionEvent, distanceX: Float, distanceY: Float) {
-                        super.onDrag(view, event, distanceX, distanceY)
-                        viewModel.translationX.value = view.translationX
-                        viewModel.translationY.value = view.translationY
-                        viewModel.dragState.value = MainActivityViewModel.DRAG
-                    }
+        binding.parent.listener = object : GestureListener() {
+            override fun onDrag(view: View, event: MotionEvent, distanceX: Float, distanceY: Float) {
+                super.onDrag(view, event, distanceX, distanceY)
+                viewModel.translationX.value = view.translationX
+                viewModel.translationY.value = view.translationY
+                viewModel.dragState.value = MainActivityViewModel.DRAG
+            }
 
-                    override fun onDragStart(view: View, event: MotionEvent) {
-                        super.onDragStart(view, event)
-                        viewModel.dragState.value = MainActivityViewModel.START
-                    }
+            override fun onDragStart(view: View, event: MotionEvent) {
+                super.onDragStart(view, event)
+                viewModel.dragState.value = MainActivityViewModel.START
+            }
 
-                    override fun onDragEnd(view: View, event: MotionEvent) {
-                        super.onDragEnd(view, event)
-                        viewModel.dragState.value = MainActivityViewModel.END
-                    }
-                }
-            ),
-            ScaleDetector(
-                object : ScaleDetector.GestureScaleListener() {
-                    override fun onScaleStart(view: View, event: MotionEvent) {
-                        super.onScaleStart(view, event)
-                        viewModel.scaleState.value = MainActivityViewModel.START
-                    }
+            override fun onDragEnd(view: View, event: MotionEvent) {
+                super.onDragEnd(view, event)
+                viewModel.dragState.value = MainActivityViewModel.END
+            }
 
-                    override fun onScale(view: View, event: MotionEvent, scale: Float) {
-                        super.onScale(view, event, scale)
-                        viewModel.scaleX.value = view.scaleX
-                        viewModel.scaleY.value = view.scaleY
-                        viewModel.scaleState.value = MainActivityViewModel.SCALE
-                    }
+            override fun onScaleStart(view: View, event: MotionEvent) {
+                super.onScaleStart(view, event)
+                viewModel.scaleState.value = MainActivityViewModel.START
+            }
 
-                    override fun onScaleEnd(view: View, event: MotionEvent) {
-                        super.onScaleEnd(view, event)
-                        viewModel.scaleState.value = MainActivityViewModel.END
-                    }
-                },
-                object : ScaleDetector.GestureRotateListener() {
-                    override fun onRotateStart(view: View, event: MotionEvent) {
-                        super.onRotateStart(view, event)
-                        viewModel.rotateState.value = MainActivityViewModel.START
-                    }
+            override fun onScale(view: View, event: MotionEvent, scale: Float) {
+                super.onScale(view, event, scale)
+                viewModel.scaleX.value = view.scaleX
+                viewModel.scaleY.value = view.scaleY
+                viewModel.scaleState.value = MainActivityViewModel.SCALE
+            }
 
-                    override fun onRotate(view: View, event: MotionEvent, rotation: Float) {
-                        super.onRotate(view, event, rotation)
-                        viewModel.rotation.value = view.rotation
-                        viewModel.rotateState.value = MainActivityViewModel.ROTATE
-                    }
+            override fun onScaleEnd(view: View, event: MotionEvent) {
+                super.onScaleEnd(view, event)
+                viewModel.scaleState.value = MainActivityViewModel.END
+            }
 
-                    override fun onRotateEnd(view: View, event: MotionEvent) {
-                        super.onRotateEnd(view, event)
-                        viewModel.rotateState.value = MainActivityViewModel.END
-                    }
-                }
-            )
-        )
+            override fun onRotateStart(view: View, event: MotionEvent) {
+                super.onRotateStart(view, event)
+                viewModel.rotateState.value = MainActivityViewModel.START
+            }
+
+            override fun onRotate(view: View, event: MotionEvent, rotation: Float) {
+                super.onRotate(view, event, rotation)
+                viewModel.rotation.value = view.rotation
+                viewModel.rotateState.value = MainActivityViewModel.ROTATE
+            }
+
+            override fun onRotateEnd(view: View, event: MotionEvent) {
+                super.onRotateEnd(view, event)
+                viewModel.rotateState.value = MainActivityViewModel.END
+            }
+        }
     }
 
     private fun onCreateTextViewOnClickListener() {
